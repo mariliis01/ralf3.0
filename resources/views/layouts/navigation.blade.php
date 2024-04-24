fb2a1efcfd3c4603fac15aae4f20038565d6a7b3
Mari-Liis <mariliis.sink@gmail.com>
2024-04-24 16:00:39 +0300

    api nav link

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -27,9 +27,10 @@
                     <x-nav-link :href="route('products.index')" :active="request()->routeIs('products.index')">
                         {{ __('E-shop') }}
                     </x-nav-link>
-                    <x-nav-link :href="https://ralf.ta22sink.itmajakas.ee/api/makeup">
+                    <x-nav-link href="https://ralf.ta22sink.itmajakas.ee/api/makeup">
                         {{ __('Api') }}
                     </x-nav-link>
+
                 </div>
             </div>
 
